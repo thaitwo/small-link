a4a23d086bd61ebbcd451ae9a870e7e3009f0792
thaitwo <tmtu90@gmail.com>
2019-08-22 14:54:25 -0700

    Updates

---
+++ precache-manifest.64c9cd6d920a021732d356444b29c3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32de13445ac20c427c07586a0aae287b",
+    "url": "/small-link/index.html"
+  },
+  {
+    "revision": "832a0048b2f4af2a1d0e",
+    "url": "/small-link/static/css/2.6cc09ab4.chunk.css"
+  },
+  {
+    "revision": "c3e0a6d15327e809e7c6",
+    "url": "/small-link/static/css/main.3690899f.chunk.css"
+  },
+  {
+    "revision": "832a0048b2f4af2a1d0e",
+    "url": "/small-link/static/js/2.01c4a814.chunk.js"
+  },
+  {
+    "revision": "c3e0a6d15327e809e7c6",
+    "url": "/small-link/static/js/main.33710c8e.chunk.js"
+  },
+  {
+    "revision": "72fd1a1771b132b2430e",
+    "url": "/small-link/static/js/runtime~main.589bd5dc.js"
+  }
+]);
